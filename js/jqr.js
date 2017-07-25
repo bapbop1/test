639ab55e5678ff3dc968cde18bfa3773a0fd7b09
bapbop1 <bapbop1@gmail.com>
2017-07-25 10:26:51 +0700

    up

---
+++ js/jqr.js
@@ -0,0 +1,34 @@
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" btn-primary", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " btn-primary";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("default").click();
+function opensile(evt, Name) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent2");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" btn-primary", "");
+    }
+    document.getElementById(Name).style.display = "block";
+    evt.currentTarget.className += " btn-primary";
+
+}
+
+// Get the element with id="defaultOpen" and click on it
+
+    document.getElementById("defaultOpen").click();
