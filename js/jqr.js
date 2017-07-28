96ce3e20e1b9a60fbc7a9bcbc0a555c0ec415d6b
nlsnam1996 <nguyenlesonnam@gmail.com>
2017-07-28 18:16:43 +0700

    h

--- js/jqr.js
+++ js/jqr.js
@@ -14,6 +14,7 @@ function openCity(evt, cityName) {
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("default").click();
+
 function opensile(evt, Name) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent2");
@@ -32,3 +33,24 @@ function opensile(evt, Name) {
 // Get the element with id="defaultOpen" and click on it
 
 document.getElementById("defaultOpen").click();
+
+$(document).ready(function(){
+    jQuery(function($) {
+        if ($(window).width() > 769) {
+            $('.navbar .dropdown').hover(function() {
+                $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+
+            }, function() {
+                $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
+
+            });
+
+            $('.navbar .dropdown > a').click(function() {
+                location.href = this.href;
+            });
+
+        }
+    });
+
+});
+
